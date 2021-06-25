ca0bf215114a6ddb3535397c312fa4634b7862c3
pramath <pramathga@gmail.com>
2021-06-25 11:13:14 +0530

    added show page

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,4 +2,12 @@ class MovieController < ApplicationController
   def index
     @movies = Movie.all
   end
+  def show 
+    # @movies = Movie.all
+    @movie = Movie.find(params[:id])
+  end
+
+  # def movie_params
+  #   params.require(:movie).permit(:Name, :Rating, :summary, :released_on)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'movie/index'
+   resources :movie
+  #  get 'movie/index'
+  #  get "/movie/:id", to: "movie#show", as: 'movie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+# get "/movies", to: "movies#index"
+  
